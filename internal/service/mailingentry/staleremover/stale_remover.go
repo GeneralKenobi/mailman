afb6593dfda3fa1a1486daa4123a56934efb9e11
Szymon Żogała <szymek.655@gmail.com>
2022-03-26 13:08:14 +0100

    Change stale entry remover to keep customers even if they have no mailing entries

--- internal/service/mailingentry/staleremover/stale_remover.go
+++ internal/service/mailingentry/staleremover/stale_remover.go
@@ -12,9 +12,7 @@ import (
 type Repository interface {
 	FindMailingEntriesOlderThan(ctx context.Context, olderThan time.Duration) ([]model.MailingEntry, error)
 	FindMailingEntriesByMailingIdOlderThan(ctx context.Context, mailingId int, olderThan time.Duration) ([]model.MailingEntry, error)
-	FindMailingEntriesByCustomerId(ctx context.Context, customerId int) ([]model.MailingEntry, error)
 	DeleteMailingEntryById(ctx context.Context, id int) error
-	DeleteCustomerById(ctx context.Context, id int) error
 }
 
 type StaleEntryRemover struct {
@@ -29,8 +27,9 @@ func New(repository Repository) *StaleEntryRemover {
 func (remover *StaleEntryRemover) RemoveByMailingId(ctx context.Context, mailingId int) error {
 	staleEntries, err := remover.repository.FindMailingEntriesByMailingIdOlderThan(ctx, mailingId, stalenessThreshold())
 	if err != nil {
-		return err
+		return fmt.Errorf("error listing stale mailing entries with mailing ID %d: %w", mailingId, err)
 	}
+
 	return remover.removeStaleEntries(ctx, staleEntries)
 }
 
@@ -38,8 +37,9 @@ func (remover *StaleEntryRemover) RemoveByMailingId(ctx context.Context, mailing
 func (remover *StaleEntryRemover) Remove(ctx context.Context) error {
 	staleEntries, err := remover.repository.FindMailingEntriesOlderThan(ctx, stalenessThreshold())
 	if err != nil {
-		return err
+		return fmt.Errorf("error listing stale mailing entries: %w", err)
 	}
+
 	return remover.removeStaleEntries(ctx, staleEntries)
 }
 
@@ -51,36 +51,6 @@ func (remover *StaleEntryRemover) removeStaleEntries(ctx context.Context, staleE
 		err := remover.repository.DeleteMailingEntryById(ctx, entry.Id)
 		if err != nil {
 			return fmt.Errorf("error removing stale entry %d: %w", entry.Id, err)
-		}
-	}
-
-	err := remover.removeNoLongerReferencedCustomers(ctx, staleEntries)
-	if err != nil {
-		return fmt.Errorf("error cleaning up customers after removing stale mailing entries: %w", err)
-	}
-	return err
-}
-
-func (remover *StaleEntryRemover) removeNoLongerReferencedCustomers(ctx context.Context, removedEntries []model.MailingEntry) error {
-	uniqueCustomerIds := make(map[int]bool)
-	for _, entry := range removedEntries {
-		uniqueCustomerIds[entry.MailingId] = true
-	}
-
-	for customerId := range uniqueCustomerIds {
-		entriesForCustomer, err := remover.repository.FindMailingEntriesByCustomerId(ctx, customerId)
-		if err != nil {
-			return fmt.Errorf("error listing mailing entries for customer %d: %err", customerId, err)
-		}
-		if len(entriesForCustomer) > 0 {
-			mdctx.Debugf(ctx, "Not cleaning up customer %d - they still have %d mailing entries", customerId, len(entriesForCustomer))
-			continue
-		}
-
-		mdctx.Infof(ctx, "Removing customer %d - no more mailing entries are associated with them", customerId)
-		err = remover.repository.DeleteCustomerById(ctx, customerId)
-		if err != nil {
-			return fmt.Errorf("error cleaning up customer %d: %w", customerId, err)
 		}
 	}
 
