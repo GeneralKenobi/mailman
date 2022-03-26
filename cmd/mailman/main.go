a16dc676f076490b8ef9b6a7fade9e342059b0da
Szymon Żogała <szymek.655@gmail.com>
2022-03-26 17:10:43 +0100

    Update and rewire main

--- cmd/mailman/main.go
+++ cmd/mailman/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"github.com/GeneralKenobi/mailman/internal/api"
+	"github.com/GeneralKenobi/mailman/internal/api/httpgin"
 	"github.com/GeneralKenobi/mailman/internal/config"
+	"github.com/GeneralKenobi/mailman/internal/email/mock"
+	"github.com/GeneralKenobi/mailman/internal/persistence/postgres"
 	"github.com/GeneralKenobi/mailman/pkg/mdctx"
 	"github.com/GeneralKenobi/mailman/pkg/shutdown"
 	"os"
@@ -13,21 +15,40 @@ import (
 
 func main() {
 	argsCfg := commandLineArgsConfig()
-
-	cfg, err := config.Load(argsCfg.configFiles)
+	err := config.Load(argsCfg.configFiles)
 	if err != nil {
 		mdctx.Fatalf(nil, "Error loading configuration: %v", err)
 	}
 
 	// TODO: Shutdown time configuration
 	parentCtx := shutdown.NewParentContext(30 * time.Second)
-	go api.ServeHttp(parentCtx.NewContext("http server"), cfg.HttpServer)
+	bootstrap(parentCtx)
+	shutdownAfterStopSignal(parentCtx)
+}
 
+func bootstrap(parentCtx shutdown.ParentContext) {
+	// DB
+	persistenceCtx, err := postgres.NewContext(parentCtx.NewContext("postgres"))
+	if err != nil {
+		mdctx.Fatalf(nil, "Error connecting to DB: %v", err)
+	}
+
+	// Email service
+	emailer := mock.NewEmailer()
+
+	// HTTP server
+	httpServer := httpgin.NewServer(persistenceCtx, emailer)
+	go httpServer.Start(parentCtx.NewContext("http server"))
+}
+
+func shutdownAfterStopSignal(parentCtx shutdown.ParentContext) {
 	stopSignalChannel := make(chan os.Signal)
 	// SIGINT for ctrl+c, SIGTERM for k8s stopping the container.
 	signal.Notify(stopSignalChannel, syscall.SIGINT, syscall.SIGTERM)
+
 	caughtSignal := <-stopSignalChannel
 	mdctx.Infof(nil, "Caught signal %v, shutting down", caughtSignal)
+
 	parentCtx.Cancel()
 	mdctx.Infof(nil, "Shutdown completed, exiting")
 }
